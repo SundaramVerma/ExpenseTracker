754d0feac06c1a4606cb045288e480b30a48c5b5
Sundaram verma <sundaram20/ug@cse.nits.ac.in>
2022-07-27 19:09:53 +0530

    expense traker

---
+++ expenseTracker-main/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Wrapper from './Components/Wrapper';
+import Style from './Style.css'
+
+function App() {
+  return (
+    <div className="App ">
+     <Wrapper/>
+    </div>
+  );
+}
+
+export default App;
